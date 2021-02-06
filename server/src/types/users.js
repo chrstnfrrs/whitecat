f58de21be323d9b025a5d0b3fc18678863aaec65
Christian Farris <christianfarriscs@gmail.com>
2021-02-05 21:45:33 -0600

    create user types

---
+++ server/src/types/users.js
@@ -0,0 +1,39 @@
+import {gql} from 'apollo-server'
+
+const userTypes = gql`
+  extend type Query {
+    user(uuid: ID!): User
+    users: [User!]
+  }
+
+  extend type Mutation {
+    createUser(input: UserInput!): User!
+    updateUser(uuid: ID!, input: UserUpdate!): User!
+    deleteUser(uuid: ID!): Boolean!
+    loginUser(input: UserInput!): LoginCredentials!
+  }
+
+  type LoginCredentials {
+    accessToken: String!
+  }
+
+  type User {
+    uuid: ID!
+    id: ID!
+    email: String!
+    password: String!
+    tokenVersion: Int!
+  }
+
+  input UserInput {
+    email: String!
+    password: String!
+  }
+
+  input UserUpdate {
+    email: String
+    password: String
+  }
+`
+
+export default userTypes
