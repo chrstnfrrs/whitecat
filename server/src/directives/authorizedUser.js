e74ded47deb3e290d271908524dbf1e3e401effe
Christian Farris <christianfarriscs@gmail.com>
2021-02-09 22:20:05 -0600

    store this logic here for later

---
+++ server/src/directives/authorizedUser.js
@@ -0,0 +1,14 @@
+// import jsonwebtoken from 'jsonwebtoken'  
+// const authorization = context.req.headers.authorization
+
+// console.log('invalid token')
+// if(!authorization) throw new Error('Not Authenticated')
+
+// try {
+//   const authorizationToken = authorization.split(' ')[1]
+//   const payload = jsonwebtoken.verify(authorizationToken, process.env.ACCESS_TOKEN_SECRET)
+//   uuid = payload.uuid
+// } catch {
+//   console.log('invalid token')
+//   throw new Error('invalid token')
+// }
