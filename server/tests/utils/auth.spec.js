6b0f2660dd583595f6648fc0ccd4d61b30ba548b
Christian Farris <christianfarriscs@gmail.com>
2021-02-09 22:20:59 -0600

    test auth utils

---
+++ server/tests/utils/auth.spec.js
@@ -0,0 +1,33 @@
+import jsonwebtoken from 'jsonwebtoken'
+import Chance from 'chance'
+
+import { createAccessToken } from '../../src/utils/auth'
+
+jest.mock('jsonwebtoken')
+
+const chance = new Chance()
+
+describe('auth utils', () => {
+  describe('createAccessToken', () => {
+    test('should sign a jwt', () => {
+      const user = {
+        uuid: chance.string(),
+        email: chance.string(),
+        tokenVersion: chance.natural()
+      }
+
+      createAccessToken(user)
+      
+      expect(jsonwebtoken.sign).toHaveBeenCalledTimes(1)
+      expect(jsonwebtoken.sign).toHaveBeenCalledWith({uuid: user.uuid, email: user.email, tokenVersion: user.tokenVersion}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15m' })
+    });
+    test('should sign a jwt', () => {
+      const accessToken = chance.string()
+      jsonwebtoken.sign.mockReturnValue(accessToken)
+
+      const result = createAccessToken(chance.string())
+      
+      expect(result).toStrictEqual(accessToken)
+    });
+  });
+});
