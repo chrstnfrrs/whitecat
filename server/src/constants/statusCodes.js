40ce88b63537f045a1e92cfc94ca435877f44da5
Christian Farris <christianfarriscs@gmail.com>
2021-02-09 22:19:55 -0600

    create httpStatus codes

---
+++ server/src/constants/statusCodes.js
@@ -0,0 +1,4 @@
+export const httpStatus = {
+  404: '404 - Not Found',
+  406: '406 - Not Acceptable'
+}
---
+++ server/tests/constants/statusCodes.spec.js
@@ -0,0 +1,11 @@
+import { httpStatus } from '../../src/constants/statusCodes'
+
+describe('statusCodes', () => {
+  describe('httpStatus', () => {
+    test('should return correct status code and message for 404 error', () => {
+      expect(httpStatus[404]).toStrictEqual('404 - Not Found')
+    });    test('should return correct status code and message for 406 error', () => {
+      expect(httpStatus[406]).toStrictEqual('406 - Not Acceptable')
+    });
+  });
+});
