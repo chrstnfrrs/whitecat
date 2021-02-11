65f61cd44b30daa3175431c322e48f39f65c4542
Christian Farris <christianfarriscs@gmail.com>
2021-02-10 18:52:38 -0600

    adjust index test for days spike resolvers

--- server/tests/resolvers/index.spec.js
+++ server/tests/resolvers/index.spec.js
@@ -8,20 +8,28 @@ import {
   loginUserResolver
 } from '../../src/resolvers/users'
 import { helloResolver } from '../../src/resolvers/hello'
+import {
+  daysResolver,
+  allDaysResolver,
+  createDayResolver
+} from '../../src/resolvers/days'
 
 describe('resolvers', () => {
   test('should be have correct resolver map', () => {
     expect(JSON.stringify(resolvers)).toEqual(JSON.stringify({
       Query: {
+        allDays: allDaysResolver,
+        days: daysResolver,
         user: userResolver,
         users: usersResolver,
-        hello: helloResolver
+        hello: helloResolver,
       },
       Mutation: {
+        createDay: createDayResolver,
         createUser: createUserResolver,
-        updateUser: updateUserResolver,
         deleteUser: deleteUserResolver,
         loginUser: loginUserResolver,
+        updateUser: updateUserResolver,
       }
     }))
   });
