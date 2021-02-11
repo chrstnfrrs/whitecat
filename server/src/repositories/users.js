531e816c10f27fa2c356fb28cef13fcee1ebbcaa
Christian Farris <christianfarriscs@gmail.com>
2021-02-10 18:51:16 -0600

    adjust users repositories to schema changes

--- server/src/repositories/users.js
+++ server/src/repositories/users.js
@@ -1,13 +1,13 @@
 import db from '../queryBuilder'
 
 const TABLE = 'users'
-const COLUMNS = ['uuid', 'id', 'email', 'password', 'tokenVersion']
+const COLUMNS = ['uuid', 'email', 'password', 'tokenVersion']
 
 export const deleteUser = async (uuid) =>
   db.table(TABLE).del().where({uuid})
 
 export const insertUser = async (user) =>
-  db.table(TABLE).insert(user).returning(COLUMNS)
+  db.table(TABLE).insert(user, COLUMNS)
 
 export const selectUserById = async (uuid) =>
   db.table(TABLE).select().where({uuid})
@@ -19,4 +19,4 @@ export const selectUsers = async () =>
   db.table(TABLE).select()
 
 export const updateUser = async (uuid, user) =>
-  db.table(TABLE).where({uuid}).update(user).returning(COLUMNS)
+  db.table(TABLE).where({uuid}).update(user, COLUMNS)
